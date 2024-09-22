d9bfa52df8eb04ff3c8a1b75d6a6830114ceadb3
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2024-09-22 21:08:11 +0530

    render deploy

--- controller/userController.js
+++ controller/userController.js
@@ -1,40 +1,85 @@
-const {Router} = require('express')
-const user = require('./../models/userModel')
-const bcrypt = require('bcryptjs')
+// const {Router} = require('express')
+// const user = require('./../models/userModel')
+// const bcrypt = require('bcryptjs')
 
-const handleRegister = async(req,res)=>{
-    const {username,email,password,confirmPassword,DOB}=req.body
-    console.log("I am here")
-    console.log(req.body)
-    if (password!=confirmPassword){
-        res.status(400).send('password dont match')
-        console.log("1")
-        return
-    }else if (!email||!password||!username||!confirmPassword||!DOB){
-        res.sendStatus(400).json({"message":"All fields are required"})
-        console.log("2")
-        return
-    }else{
-        console.log("3")
-        const userDB = await user.findOne({email});
-        if (userDB){
-            console.log(userDB)
-            res.json("redirect to login")
-            return
-        }else{
-            try {
-                console.log("here")
-                const hpassword = await bcrypt.hash(password,10)
-                const newUser = await user.create({username,email,hpassword,DOB})
-                newUser.save()
-                console.log(newUser)
-                res.sendStatus(201)
-            } catch (error) {
-                res.sendStatus(500).json({'message':error.message})
-            }
+// const handleRegister = async(req,res)=>{
+//     const {username,email,password,confirmPassword,DOB}=req.body
+//     console.log("I am here")
+//     console.log(req.body)
+//     if (password!=confirmPassword){
+//         res.status(400).send('password dont match')
+//         console.log("1")
+//         return
+//     }else if (!email||!password||!username||!confirmPassword||!DOB){
+//         res.sendStatus(400).json({"message":"All fields are required"})
+//         console.log("2")
+//         return
+//     }else{
+//         console.log("3")
+//         const userDB = await user.findOne({email});
+//         if (userDB){
+//             console.log(userDB)
+//             res.json("redirect to login")
+//             return
+//         }else{
+//             try {
+//                 console.log("here")
+//                 const hpassword = await bcrypt.hash(password,10)
+//                 const newUser = await user.create({username,email,hpassword,DOB})
+//                 newUser.save()
+//                 console.log(newUser)
+//                 res.sendStatus(201)
+//             } catch (error) {
+//                 res.sendStatus(500).json({'message':error.message})
+//             }
             
+//         }
+//     }
+// }
+
+// module.exports = {handleRegister};
+
+
+const { Router } = require('express');
+const user = require('./../models/userModel');
+const bcrypt = require('bcryptjs');
+
+const handleRegister = async (req, res) => {
+    const { username, email, password, confirmPassword, DOB } = req.body;
+    console.log("I am here");
+    console.log(req.body);
+
+    // Password mismatch check
+    if (password !== confirmPassword) {
+        console.log("1");
+        return res.status(400).send('Passwords do not match');
+    }
+
+    // Missing required fields check
+    if (!email || !password || !username || !confirmPassword || !DOB) {
+        console.log("2");
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    console.log("3");
+    const userDB = await user.findOne({ email });
+    
+    // If user already exists, redirect to login
+    if (userDB) {
+        console.log(userDB);
+        return res.status(200).json("redirect to login");
+    } else {
+        try {
+            console.log("here");
+            const hpassword = await bcrypt.hash(password, 10); // Hash password
+            const newUser = new user({ username, email, hpassword, DOB }); // Create new user object
+            await newUser.save(); // Save user to DB
+            console.log(newUser);
+            return res.sendStatus(201); // User created successfully
+        } catch (error) {
+            return res.status(500).json({ message: error.message }); // Return server error
         }
     }
-}
+};
 
-module.exports = {handleRegister};
+module.exports = { handleRegister };
--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT||3000;
 const cors = require('cors')
 const mongoose = require('mongoose')
 const path = require('path')
-const CONNECTION_STRING = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
-
+const CONNECTION_STRING = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0&tls=true`;
+console.log(process.env.MONGO_USERNAME, process.env.MONGO_CLUSTER);
 mongoose.connect(CONNECTION_STRING)
 .then(
     console.log("connected")
@@ -26,20 +26,20 @@ app.use('/rn', express.static(path.join(__dirname, '')));
 app.use('/googlefile',express.static(path.join(__dirname,'')))
 
 
-// app.use('/register',require('./router/register'))
-// app.use('/auth',require('./router/auth'))
-// app.use('/refresh',require('./router/refresh'))
-// app.use('/logout',require('./router/logout'))
-// app.use('/form',require('./router/form'))
-// app.use('/getresune',require('./router/resume'))
-// app.use('/resumetemplates',require('./router/resumeTemplates'))
-app.use('/harshdeepkv/register', require('./router/register'));
-app.use('/harshdeepkv/auth', require('./router/auth'));
-app.use('/harshdeepkv/refresh', require('./router/refresh'));
-app.use('/harshdeepkv/logout', require('./router/logout'));
-app.use('/harshdeepkv/form', require('./router/form'));
-app.use('/harshdeepkv/getresume', require('./router/resume'));
-app.use('/harshdeepkv/resumetemplates', require('./router/resumeTemplates'));
+app.use('/register',require('./router/register'))
+app.use('/auth',require('./router/auth'))
+app.use('/refresh',require('./router/refresh'))
+app.use('/logout',require('./router/logout'))
+app.use('/form',require('./router/form'))
+app.use('/getresune',require('./router/resume'))
+app.use('/resumetemplates',require('./router/resumeTemplates'))
+// app.use('/harshdeepkv/register', require('./router/register'));
+// app.use('/harshdeepkv/auth', require('./router/auth'));
+// app.use('/harshdeepkv/refresh', require('./router/refresh'));
+// app.use('/harshdeepkv/logout', require('./router/logout'));
+// app.use('/harshdeepkv/form', require('./router/form'));
+// app.use('/harshdeepkv/getresume', require('./router/resume'));
+// app.use('/harshdeepkv/resumetemplates', require('./router/resumeTemplates'));
 
 app.listen(PORT,()=>{
     console.log(`server listening on port ${PORT}`)
